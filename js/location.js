89e71a8152939228051f6a606002f3624ea943eb
razouck <razouck@users.noreply.github.com>
2024-09-19 22:32:02 -0300

    Align longitude

--- js/location.js
+++ js/location.js
@@ -2,7 +2,7 @@ function init()
 {
 	const options = {};
 
-	options.center             = [ 41.485534, -71.308221 ];
+	options.center             = [ 41.485534, -71.308134 ];
 	options.zoom               = 16;
 	options.attributionControl = false;
 	options.zoomControl        = false;
